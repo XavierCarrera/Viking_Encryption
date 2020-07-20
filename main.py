7d255a4e40a167aa50c68ca369dd5505d3759714
Xavier Carrera <xavier.carrera1990@gmail.com>
2020-07-19 22:07:22 -0500

    First framework

---
+++ main.py
@@ -0,0 +1,58 @@
+import random
+
+VIKING_SCRIPT = ["ᚠ", "ᚢ", "ᚦ", "ᚩ", "ᚱ", "ᚳ", "ᚷ", "ᚹ", "ᚺ", "ᚾ", "ᛁ", "ᛃ", "ᛇ", "ᛈ", "ᛉ", "ᛋ", "ᛏ", "ᛒ", "ᛖ", "ᛗ", "ᛚ", "ᛜ", "ᛟ", "ᛞ", "ᚪ", "ᚫ", "ᚣ", "ᛡ", "ᛠ"]
+
+class Encryption:
+
+    def __init__(self, user_input):
+        self.user_input = user_input
+    
+    def message(self):
+        
+        saved_message = []
+        saved_message.append(user_message)
+        print(saved_message[0])
+    
+    def viking_encryption(self):
+
+        runic = random.sample(VIKING_SCRITP, 10)
+        runic_string = ""
+
+        for i in runic:
+            runic_string += i
+
+        print(f"Your encrypted message is {runic_string}")
+
+if __name__ == "__main__":
+    
+    user_message = input("What message you wish to encrypt? ")
+    unencrypted = Encryption(user_message)
+    unencrypted.viking_encryption()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
