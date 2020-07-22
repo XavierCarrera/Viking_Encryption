94ca017a62d5ab922f30af6a9cb286ff3efadda3
Xavier Carrera <xavier.carrera1990@gmail.com>
2020-07-21 22:05:48 -0500

    csv integration

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 import random
 import csv
 
-VIKING_SCRIPT = ["ᚠ", "ᚢ", "ᚦ", "ᚩ", "ᚱ", "ᚳ", "ᚷ", "ᚹ", "ᚺ", "ᚾ", "ᛁ", "ᛃ", "ᛇ", "ᛈ", "ᛉ", "ᛋ", "ᛏ", "ᛒ", "ᛖ", "ᛗ", "ᛚ", "ᛜ", "ᛟ", "ᛞ", "ᚪ", "ᚫ", "ᚣ", "ᛡ", "ᛠ"]
+VIKING_SCRIPT = ["ᚠ", "ᚢ", "ᚦ", "ᚩ", "ᚱ", "ᚳ", "ᚷ", "ᚹ", "ᚺ", "ᚾ", "ᛁ", "ᛃ", "ᛇ", "ᛈ", "ᛉ", 
+"ᛋ", "ᛏ", "ᛒ", "ᛖ", "ᛗ", "ᛚ", "ᛜ", "ᛟ", "ᛞ", "ᚪ", "ᚫ", "ᚣ", "ᛡ", "ᛠ"]
 
-
-class Encryption:
+class Encryption():
 
     def __init__(self, user_input):
         self.user_input = user_input
     
-    def message(self):
-        
-        saved_message = []
-        saved_message.append(user_message)
-        print(saved_message[0])
     
     def viking_encryption(self):
 
@@ -24,37 +19,21 @@ class Encryption:
             runic_string += i
 
         print(f"Your encrypted message is {runic_string}")
+        
+        return runic_string
+        
 
 if __name__ == "__main__":
     
     user_message = input("What message you wish to encrypt? ")
     unencrypted = Encryption(user_message)
     unencrypted.viking_encryption()
+
+    with open("Viking_Record.txt", mode='w') as csv_file:
+        fieldnames = ["Priest message", "Puritan message"]
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+        writer.writeheader()
+        writer.writerow({"Priest message": user_message , "Puritan message": unencrypted })
+
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
