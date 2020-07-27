bb87487fd6ebf4ca3f277e0d62b09068e5ff40d1
Xavier Carrera <xavier.carrera1990@gmail.com>
2020-07-26 22:21:04 -0500

    First christian string implementation

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import random
 import csv
 
-# VIKING_SCRIPT = ["ᚠ", "ᚢ", "ᚦ", "ᚩ", "ᚱ", "ᚳ", "ᚷ", "ᚹ", "ᚺ", "ᚾ", "ᛁ", "ᛃ", "ᛇ", "ᛈ", "ᛉ", "ᛋ", "ᛏ", "ᛒ", "ᛖ", "ᛗ", "ᛚ", "ᛜ", "ᛟ", 
-# "ᛞ", "ᚪ", "ᚫ", "ᚣ", "ᛡ", "ᛠ"]
+VIKING_SCRIPT = ["ᚠ", "ᚢ", "ᚦ", "ᚩ", "ᚱ", "ᚳ", "ᚷ", "ᚹ", "ᚺ", "ᚾ", "ᛁ", "ᛃ", "ᛇ", "ᛈ", "ᛉ", "ᛋ", "ᛏ", "ᛒ", "ᛖ", "ᛗ", "ᛚ", "ᛜ", "ᛟ", 
+"ᛞ", "ᚪ", "ᚫ", "ᚣ", "ᛡ", "ᛠ"]
 
-VIKING_SCRIPT = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-
-CHRISTIAN_SCRIPT = []
+CHRISTIAN_SCRIPT = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
 class Encryption():
 
@@ -27,7 +25,24 @@ class Encryption():
 
         print(f"Your encrypted message is {self.runic_string}")
 
-def Viking_Memory():
+class Record():
+    
+    def __init__(self, christian_string =""):
+        self.christian_string = christian_string
+    
+    def __str__(self):
+        return "{}".format(self.christian_string)
+    
+    def christian_record(self, christian_string = ""):
+
+        christian = random.sample(CHRISTIAN_SCRIPT, 5)
+
+        for i in christian:
+            self.christian_string += i
+
+        print(self.christian_string)
+
+def viking_memory():
 
     with open('Viking_Record.txt', mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
@@ -43,13 +58,14 @@ if __name__ == "__main__":
     unencrypted = Encryption(user_message)
     unencrypted.viking_encryption()
 
-    Viking_Memory()
+    christian_memory = Record()
+    christian_memory.christian_record()
 
     with open("Viking_Record.txt", mode='w') as csv_file:
         fieldnames = ["Priest message", "Puritan message"]
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
         writer.writeheader()
-        writer.writerow({"Priest message": user_message, "Puritan message" : unencrypted})
+        writer.writerow({"Priest message": user_message, "Puritan message" : christian_memory})
 
     
